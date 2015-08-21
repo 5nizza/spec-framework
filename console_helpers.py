f0411269263a8dad2b980a3a527386fdb31ce342
lprikler <leo.prikler@student.tugraz.at>
2015-08-21 15:14:06 +0200

    increase performance on high usage of print_red and print_green

--- console_helpers.py
+++ console_helpers.py
@@ -1,21 +1,16 @@
 """
 Reqiures colorama package.
 """
+try:
+    from colorsma import init, Fore
 
+    init(autoreset=True)
 
-def print_red(*o):
-    try:
-        from colorama import init, Fore
-        init(autoreset=True)
+    def print_red(*o):
         print(Fore.RED + str(list(o)[0]), *list(o)[1:])
-    except ImportError:
-        print(*o)
 
-
-def print_green(*o):
-    try:
-        from colorama import init, Fore
-        init(autoreset=True)
+    def print_green(*o):
         print(Fore.GREEN + str(list(o)[0]), *list(o)[1:])
-    except ImportError:
-        print(*o)
+
+except ImportError:
+    print_red = print_green = lambda *o: print(o)

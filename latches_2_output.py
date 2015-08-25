36bf9640227c08ba1d7dc6f32458604e515de60f
lprikler <leo.prikler@student.tugraz.at>
2015-08-25 11:46:09 +0200

    refactoring

--- latches_2_output.py
+++ latches_2_output.py
@@ -1,14 +1,22 @@
 #! /usr/bin/env python2.7
 
 from argparse import ArgumentParser
+from re import match
 
 import aiger_swig.aiger_wrap as aiglib
 
 
-bad_names = ["bad_variable", "bad_var"]
-constraint_names = ["constraint_variable", "constr_variable", "constraint_var", "constr_var"]
-justice_names = ["justice_variable", "justice_var", "just_var", "just_variable"]
-fair_names = ["fair_variable", "fair_var"]
+"""
+Tool to convert 'latches' with specific names to corresponding AIGER signals.
+"""
+
+skippable = "[A-Za-z0-9_]*"
+end = "\Z"
+string_to_match = "(just(ice)?|bad|constr(aint)?|fair)_(var(iable)?|sig(nal)?)"
+regex = "({skip}_)?{stm}(_{skip})?{end}".format(skip=skippable,
+                                        stm=string_to_match,
+                                        end=end)
+
 
 def main(filename):
     model = aiglib.aiger_init()
@@ -19,20 +27,27 @@ def main(filename):
 
     for i in range(model.num_latches):
         latch = aiglib.get_aiger_symbol(latches, i)
-        if any(name in latch.name for name in bad_names):
-            aiglib.aiger_add_bad(model, latch.lit, latch.name)
-        if any(name in latch.name for name in constraint_names):
-            aiglib.aiger_add_constraint(model, latch.lit, latch.name)
-        if any(name in latch.name for name in justice_names):
-            aiglib.aiger_add_justice(model, 1, [latches.lit], latch.name)
-        if any(name in latch.name for name in fair_names):
-            aiglib.aiger_add_fairness(model, latch.lit, latch.name)
+        m = match(regex, latch.name)
+        if m:
+            ltype = m.groups()[1][0]
+            if ltype == "b":
+                aiglib.aiger_add_bad(model, latch.lit, latch.name)
+            if ltype == "c":
+                aiglib.aiger_add_constraint(model, latch.lit, latch.name)
+            if ltype == "j":
+                aiglib.aiger_add_justice(model, 1, [latch.lit], latch.name)
+            if ltype == "f":
+                aiglib.aiger_add_fairness(model, latch.lit, latch.name)
 
-    res, string = aiglib.aiger_write_to_string(model, aiglib.aiger_ascii_mode, 2147483648)
+
+    res, string = aiglib.aiger_write_to_string(model,
+                                               aiglib.aiger_ascii_mode,
+                                               2147483648)
 
     assert res != 0, 'writing failure'
 
     print string
+
 
 if __name__ == "__main__":
     parser = ArgumentParser()

95edd669316858439dc1c6e71e00c10176e9bb76
ayrat <ayrat.khalimov@gmail.com>
2015-09-17 11:44:32 +0500

    removed old files

--- tests/kliveness/produce-old-format.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for f in `ls ../*.aag`; 
-do 
-  ~/projects/spec-framework/justice_2_safety.py $f $f.klive --k 10;
-done;
--- tests/kliveness/run-safety-all-here.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for f in `find . -name "*.klive"`; 
-do 
-echo $f;
-~/projects/aisy-classroom-akdv14/aisy.py -r $f; 
-done;
